f0efe04151ffbeb1cd81836a0598c0d0363c9ea6
sushichan044 <mail@sushichan.live>
2025-03-15 01:03:57 +0900

    共通レイアウトお引越し

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import { Link } from "react-router";
+import Fa6BrandsGithub from "~icons/fa6-brands/github";
+import Fa6BrandsXTwitter from "~icons/fa6-brands/x-twitter";
 
 import type { Route } from "./+types/root";
 
+import { cn } from "./lib/cn";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
@@ -34,6 +38,36 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <header
+          className={cn(
+            "grid grid-cols-[1fr_auto_auto] gap-4 md:gap-6",
+            "font-bold text-2xl",
+            "container mx-auto p-4",
+          )}
+        >
+          <Link
+            className="hover:opacity-70 focus:opacity-70 transition duration-300"
+            to="/"
+          >
+            sushichan044&apos;s UI Lab
+          </Link>
+          <Link
+            className="hover:opacity-70 focus:opacity-70 transition duration-300"
+            target="_blank"
+            to="https://x.com/sushichan044"
+          >
+            <span className="sr-only">Follow me on Twitter</span>
+            <Fa6BrandsXTwitter aria-hidden />
+          </Link>
+          <Link
+            className="hover:opacity-70 focus:opacity-70 transition duration-300"
+            target="_blank"
+            to="https://github.com/sushichan044/react-lab"
+          >
+            <span className="sr-only">Show this project on GitHub</span>
+            <Fa6BrandsGithub aria-hidden />
+          </Link>
+        </header>
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,6 @@
 import { Link } from "react-router";
-import Fa6BrandsGithub from "~icons/fa6-brands/github";
-import Fa6BrandsXTwitter from "~icons/fa6-brands/x-twitter";
 
 import type { Route } from "./+types/_index";
-
-import { cn } from "../lib/cn";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -21,40 +17,15 @@ export function loader({ context }: Route.LoaderArgs) {
   return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
 }
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+export default function Home() {
   return (
-    <>
-      <header
-        className={cn(
-          "grid grid-cols-[1fr_auto_auto] gap-4 md:gap-6",
-          "font-bold text-2xl",
-          "container mx-auto p-4",
-        )}
-      >
-        <Link
-          className="hover:opacity-70 focus:opacity-70 transition duration-300"
-          to="/"
-        >
-          sushichan044&apos;s UI Lab
+    <main className="mx-auto container p-4">
+      <section>
+        <h2 className="text-xl font-bold">Input</h2>
+        <Link to="/input/checkbox">
+          <p>Checkbox</p>
         </Link>
-        <Link
-          className="hover:opacity-70 focus:opacity-70 transition duration-300"
-          target="_blank"
-          to="https://x.com/sushichan044"
-        >
-          <span className="sr-only">Follow me on Twitter</span>
-          <Fa6BrandsXTwitter aria-hidden />
-        </Link>
-        <Link
-          className="hover:opacity-70 focus:opacity-70 transition duration-300"
-          target="_blank"
-          to="https://github.com/sushichan044/react-lab"
-        >
-          <span className="sr-only">Show this project on GitHub</span>
-          <Fa6BrandsGithub aria-hidden />
-        </Link>
-      </header>
-      <main className="mx-auto container p-4">{loaderData.message}</main>
-    </>
+      </section>
+    </main>
   );
 }
