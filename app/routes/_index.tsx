ae7c2f92fb106341cb21ce88b0408ac0f771b43a
sushichan044 <mail@sushichan.live>
2025-03-15 00:25:36 +0900

    setup cn function

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router";
+import Fa6BrandsGithub from "~icons/fa6-brands/github";
+import Fa6BrandsXTwitter from "~icons/fa6-brands/x-twitter";
 
 import type { Route } from "./+types/_index";
+
+import { cn } from "../lib/cn";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -20,11 +24,37 @@ export function loader({ context }: Route.LoaderArgs) {
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <>
-      <header className="mx-auto container p-4 font-bold text-2xl flex justify-between flex-row">
-        <Link to="/">sushichan044&apos;s UI Lab</Link>
-        <div>a</div>
+      <header
+        className={cn(
+          "grid grid-cols-[1fr_auto_auto] gap-4 md:gap-6",
+          "font-bold text-2xl",
+          "container mx-auto p-4",
+        )}
+      >
+        <Link
+          className="hover:opacity-70 focus:opacity-70 transition duration-300"
+          to="/"
+        >
+          sushichan044&apos;s UI Lab
+        </Link>
+        <Link
+          className="hover:opacity-70 focus:opacity-70 transition duration-300"
+          target="_blank"
+          to="https://x.com/sushichan044"
+        >
+          <span className="sr-only">Follow me on Twitter</span>
+          <Fa6BrandsXTwitter aria-hidden />
+        </Link>
+        <Link
+          className="hover:opacity-70 focus:opacity-70 transition duration-300"
+          target="_blank"
+          to="https://github.com/sushichan044/react-lab"
+        >
+          <span className="sr-only">Show this project on GitHub</span>
+          <Fa6BrandsGithub aria-hidden />
+        </Link>
       </header>
-      <main>{loaderData.message}</main>
+      <main className="mx-auto container p-4">{loaderData.message}</main>
     </>
   );
 }
